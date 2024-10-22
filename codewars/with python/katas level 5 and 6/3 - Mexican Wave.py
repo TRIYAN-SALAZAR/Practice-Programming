b335aba950d9d9421df1bf40e115acde739404bc
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2024-10-22 10:02:44 -0600

    Problema Wave

---
+++ codewars/with python/katas level 5 and 6/3 - Mexican Wave.py
@@ -0,0 +1,18 @@
+def wave(people):
+    wave = []
+    for i in range(0, len(people)):
+        string = ''
+        for j, char in enumerate(people):
+            
+            if i == j:
+                string += char.upper()
+            else:
+                string += char
+            
+            if j == len(people) - 1 and people[i] != ' ':
+                wave.append(string)
+        print(string)
+    return wave
+
+
+print(wave("gap"))
